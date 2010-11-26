020693468af6dad0e1b30d324a0dd66ad7953e14
hubmartin <BFU@.(none)>
2010-11-26 02:26:52 +0100

    Initial version

---
+++ helper.js
@@ -0,0 +1,75 @@
+﻿// JavaScript Document
+	
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(index) {
+	/*
+	if(this.length == 1)
+	{
+		this.length = 0;
+		return;
+	} */
+	  this.splice(index, 1);
+};  
+
+
+
+
+
+        var Dom = {
+  get: function(el) {
+    if (typeof el === 'string') {
+      return document.getElementById(el);
+    } else {
+      return el;
+    }
+  },
+  add: function(el, dest) {
+    var el = this.get(el);
+    var dest = this.get(dest);
+    dest.appendChild(el);
+  },
+  remove: function(el) {
+    var el = this.get(el);
+    el.parentNode.removeChild(el);
+  }
+  };
+  var Event = {
+  add: function() {
+    if (window.addEventListener) {
+      return function(el, type, fn) {
+        Dom.get(el).addEventListener(type, fn, false);
+      };
+    } else if (window.attachEvent) {
+      return function(el, type, fn) {
+        var f = function() {
+          fn.call(Dom.get(el), window.event);
+        };
+        Dom.get(el).attachEvent('on' + type, f);
+      };
+    }
+  }()
+ };
+ 
+ 
+ 
+ 
+ // implement JSON.stringify serialization
+JSON.stringify = JSON.stringify || function (obj) {
+	var t = typeof (obj);
+	if (t != "object" || obj === null) {
+		// simple data type
+		if (t == "string") obj = '"'+obj+'"';
+		return String(obj);
+	}
+	else {
+		// recurse array or object
+		var n, v, json = [], arr = (obj && obj.constructor == Array);
+		for (n in obj) {
+			v = obj[n]; t = typeof(v);
+			if (t == "string") v = '"'+v+'"';
+			else if (t == "object" && v !== null) v = JSON.stringify(v);
+			json.push((arr ? "" : '"' + n + '":') + String(v));
+		}
+		return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+	}
+};
